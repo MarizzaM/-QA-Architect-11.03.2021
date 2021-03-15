5ff689d9451eb02c6cc795e9fe96833c7562cf1b
MarizzaM <75089445+MarizzaM@users.noreply.github.com>
2021-03-15 17:54:12 +0200

    Update Car.java

--- Car/Car.java
+++ Car/Car.java
@@ -10,12 +10,12 @@ import java.time.LocalDateTime;
 @Getter
 @Setter
 public class Car implements Comparable<Car>{
-    private String m_id;
-    private String m_brand;
-    private String m_model;
-    private LocalDateTime m_creation;
-    private String m_color;
-    private Integer seats;
+    protected String m_id;
+    protected String m_brand;
+    protected String m_model;
+    protected LocalDateTime m_creation;
+    protected String m_color;
+    protected Integer seats;
 
     public Car(String m_id, String m_brand, String m_model, LocalDateTime m_creation, String m_color, Integer seats) {
         this.m_id = m_id;
